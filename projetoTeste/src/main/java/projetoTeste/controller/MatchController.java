ad41bf539dd2288f21b015c461664de35becc647
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-03-25 16:25:22 -0300

    criando end-point de soma

---
+++ projetoTeste/src/main/java/projetoTeste/controller/MatchController.java
@@ -0,0 +1,34 @@
+package projetoTeste.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/match")
+public class MatchController {
+	
+	@RequestMapping("/sum/{numberOne}/{numbertwo}")
+	public Double sum(
+			@PathVariable("numberOne") String numberOne,
+			@PathVariable("numbertwo") String numbertwo) {
+		
+		if(!isNumeric(numberOne) || !isNumeric(numbertwo) ) throw new IllegalArgumentException();
+		
+		return converterDouble(numberOne) +converterDouble(numbertwo) ;
+		
+	}
+
+	private Double converterDouble(String srtNumber) {
+		if(srtNumber == null || srtNumber.isEmpty()) throw new IllegalArgumentException();
+		return Double.valueOf(srtNumber);
+	}
+
+	private boolean isNumeric(String srtNumber) {
+		if(srtNumber == null || srtNumber.isEmpty()) return false;
+		String number = srtNumber.replace(",", ".");
+		
+		 return number.matches("[-+]?^[0-9]*\\.?[0-9]+");
+	}
+
+}
